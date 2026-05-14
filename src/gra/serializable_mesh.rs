555599612ecaa1575b0a2c1367fcfeb4c7e10756
skokovin <s.kokovin@gmail.com>
2026-05-14 20:40:50 +0300

    a4

--- src/gra/serializable_mesh.rs
+++ src/gra/serializable_mesh.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -6,4 +7,9 @@ pub struct SerializableMesh {
     pub normals: Vec<f32>,
     pub indices: Vec<u32>,
     pub extrude_dir: [f32; 3],
+}
+
+pub fn unpack_mesh_map(data: &[u8])-> Result<HashMap<i64, (Option<SerializableMesh>, Option<SerializableMesh>)>, postcard::Error>{
+    let result: Result<HashMap<i64, (Option<SerializableMesh>, Option<SerializableMesh>)>, postcard::Error> = postcard::from_bytes(data);
+    result
 }
